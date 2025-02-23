93cdbc2b110a0bd9c942b0772342109e4b85b762
Remi Yacoub <remi.yacoub6@gmail.com>
2025-02-23 12:24:20 +0100

    CHANGES UNPROJECT

--- unprojet/src/main/java/com/project/unprojet/controller/CloudVendorAPIService.java
+++ unprojet/src/main/java/com/project/unprojet/controller/CloudVendorAPIService.java
@@ -1,16 +1,40 @@
 package com.project.unprojet.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import com.project.unprojet.model.CloudVendor;
 
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudVendorAPIService 
 {
+    CloudVendor cloudVendor;
+    
     @GetMapping("{vendorId}")
     public CloudVendor getCloudVendorDetails(String vendorId) {
-        return new CloudVendor("C1", "Vendor 1", "Adress One", "06xxxxxxxx");
+        //  return new CloudVendor("C1", "Vendor 1", "Adress One", "06xxxxxxxx");
+        return cloudVendor;
     }
-}
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return  "Cloud Vendor created successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return  "Cloud Vendor updated successfully";
+    }
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId) {
+        this.cloudVendor = null;
+        return  "Cloud Vendor deleted with susccess";
+    }
+}   
